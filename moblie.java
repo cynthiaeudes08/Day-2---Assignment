f602ce0197d71d8ebca36a7c4014340d3d34eb70
cynthiaeudes08 <123589375+cynthiaeudes08@users.noreply.github.com>
2023-01-27 22:57:06 +0530

    Add files via upload

---
+++ moblie.java
@@ -0,0 +1,25 @@
+package assingment2;
+
+public class moblie {
+	public static void main(String[] args) {
+		String mobileBrandName= " Samsung";
+		System.out.println(" Moblie Brand Name : "+mobileBrandName);
+		char mobileLogo= 's';
+		System.out.println(" Moblie Logo : "+mobileLogo);
+		short noOfMobilePieace= 1;
+		System.out.println(" NO of mobile; "+ noOfMobilePieace);
+		int modelNumber= 78456;
+		System.out.println(" Model NUmber: "+modelNumber);
+		long mobileImeiNumber= 9876543210L;
+		System.out.println(" Imei Number: "+mobileImeiNumber);
+		float mobilePrice= 20500f;
+		System.out.println(" Mobile Price: "+mobilePrice);
+		boolean damaged= false;
+		System.out.println(" Damaged : "+damaged);
+		
+		
+		
+		
+	}
+
+}
---
+++ twoWheeler.java
@@ -0,0 +1,22 @@
+package assingment2;
+
+public class twoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels= 2;
+		System.out.println(" There are two wheels: "+noOfWheels);
+		short noOfMirror= 4;
+		System.out.println(" There are two mirror: "+noOfMirror);
+		long chassisNumber= 123456789L;
+		System.out.println(" Chassis Number : "+chassisNumber);
+		boolean punctured= true;
+		System.out.println(" Two wheeler is punctured: "+punctured);
+		String bikeName= "TVS";
+		System.out.println(" Bike Name : "+bikeName);
+		double runningKM= 2.5;
+		System.out.println(" Running KM : "+runningKM);
+		
+	}
+	
+
+
+}
